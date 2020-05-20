4f7ac5f8ef840660bc6a6c5c6a11d10234529714
DF-Huiling <duvalhuiling@gmail.com>
2020-05-20 12:08:00 +0200

    login_logout_merge

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Campus;
 use App\Entity\Participant;
+use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ParticipantController extends AbstractController
 {
