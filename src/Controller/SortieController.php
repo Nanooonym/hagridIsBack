39413032a112976ff4dad9cc7332584be784980b
Richard <maudet.c.r@gmail.com>
2020-05-26 11:02:56 +0200

    creer annulerSortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,9 +5,9 @@ namespace App\Controller;
 use App\Entity\Etat;
 use App\Entity\Sortie;
 use App\Entity\SortieFilter;
-use App\Entity\Ville;
 use App\Form\SortieFilterType;
 use App\Form\SortieType;
+use App\Form\AnnulerSortieType;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+
 
 /**
  * @Route("/sortie")
@@ -130,5 +131,24 @@ class SortieController extends AbstractController
         return $this->redirectToRoute('sortie_index');
     }
 
+    /**
+     * @Route("/{id}/annuler", name="annuler", methods={"GET","POST"})
+     *
+     */
+    public function annuler(Sortie $sortie, EntityManagerInterface $em, Request $request):Response
+    {
+        $form = $this->createForm(AnnulerSortieType::class, $sortie);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sortie->getEtat()->setLibelle("Annulée");
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'Votre sortie "' . $sortie->getNom() . '" est maintenant annulée');
+
+            return $this->redirectToRoute('sortie_index');
+        }
+
+
+        return $this->render('sortie/annulerSortie', ["sortie" => $sortie, "form"=> $form->createView()]);
+    }
 }
